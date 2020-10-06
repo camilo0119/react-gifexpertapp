89413afb3774b6ebac602c12b9e7972c80330a41
camilo_0119 <ccastro@mutualser.org>
2020-10-05 23:56:56 -0500

    Docs agregado

---
+++ docs/precache-manifest.f05b2570ca142c20dfb9aa3ac16a0d56.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25b893a3781d58c808e9fa4c1ce69835",
+    "url": "/index.html"
+  },
+  {
+    "revision": "da43bb77220306fad82d",
+    "url": "/static/css/main.ee17abcd.chunk.css"
+  },
+  {
+    "revision": "2fa4b8f8395c8b58cbf3",
+    "url": "/static/js/2.7930ad69.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7930ad69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da43bb77220306fad82d",
+    "url": "/static/js/main.92bbfe2c.chunk.js"
+  },
+  {
+    "revision": "72e4b1f2ff4246389bc8",
+    "url": "/static/js/runtime-main.a50afc49.js"
+  }
+]);
